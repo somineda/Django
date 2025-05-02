1f82114a294090ac3171451f03504430da03c76a
somy <fuureelf@naver.com>
2025-05-02 23:33:53 +0900

    ✨feat: chapter_07 과제 업로드

--- todo/admin.py
+++ todo/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
-from todo.models import Todo
+from todo.models import Todo, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Todo)
-class TodoAdmin(admin.ModelAdmin):
+class TodoAdmin(SummernoteModelAdmin):
     list_display = ('title', 'description', 'is_completed', 'start_date', 'end_date')
     list_filter = ('is_completed',)
     search_fields = ('title',)
     ordering = ('start_date',)
+    summernote_fields = ('description',)
+
     fieldsets = (
         ('Todo Info', {
-            'fields': ('title', 'description', 'is_completed'),
+            'fields': ('user', 'title', 'description', 'is_completed')
         }),
-        ('Date Range', {
-            'fields': ('start_date', 'end_date'),
+        ('Date & Image', {
+            'fields': ('start_date', 'end_date', 'completed_image')
         }),
     )
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('todo', 'user', 'message', 'created_at')
+    search_fields = ('message',)
+    ordering = ('-created_at',)
--- todo/forms.py
+++ todo/forms.py
@@ -1,17 +1,36 @@
 from django import forms
 from todo.models import Todo, Comment
+from django_summernote.widgets import SummernoteWidget
 
 
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ["title", "description", "start_date", "end_date"]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': SummernoteWidget(),
+            'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+        }
 
 
 class TodoUpdateForm(forms.ModelForm):
+    is_completed = forms.BooleanField(
+        required=False,
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
+
     class Meta:
         model = Todo
-        fields = ["title", "description", "start_date", "end_date", "is_completed"]
+        fields = ["title", "description", "start_date", "end_date", "is_completed", "completed_image"]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': SummernoteWidget(),
+            'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'completed_image': forms.FileInput(attrs={'class': 'form-control'}),
+        }
 
 
 class CommentForm(forms.ModelForm):
